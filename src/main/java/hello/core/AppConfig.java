b3a6b1b30ffa925a3835fb7ca777ad90032ca58f
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-13 01:26:49 +0900

    test: spring container ensures singleton

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -18,16 +18,19 @@ public class AppConfig {
     //이건 메서드
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
